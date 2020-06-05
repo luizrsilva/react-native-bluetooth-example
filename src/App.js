5507db31667667bdb5a9d4cdcfad7d9680519046
saollime <saollime@SAOM4156>
2020-06-05 08:59:48 -0300

    Add initial project structure

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Home from './pages/Home';
+import DeviceDetails from './pages/DeviceDetails';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DeviceDetails} />
+      </Stack.Navigator>
+    </NavigationContainer>);
+};
+
+export default App; 
